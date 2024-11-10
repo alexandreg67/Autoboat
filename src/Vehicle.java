95101a89257e7e694eb3235b8e8d568bdc5cfdef
alex <alexandre.ag.67@gmail.com>
2024-11-10 15:24:08 +0100

    Ajout du constructeur pour initialiser brand et kilometers dans Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,4 +3,9 @@ public abstract class Vehicle {
     private String brand;
     private int kilometers;
 
+    public Vehicle(String brand, int kilometers) {
+        this.brand = brand;
+        this.kilometers = kilometers;
+    }
+
 }
